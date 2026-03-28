20d4d2a95ba8ddd32ed7a5943a9645618fcdbddd
Jeffer512 <140467995+Jeffer512@users.noreply.github.com>
2026-03-28 16:22:00 -0500

    update vite.config.js for deployment on github pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://Jeffer512.github.io/react-movie-database-app',
+  base: 'https://jeffer512.github.io/react-movie-database-app',
 })
