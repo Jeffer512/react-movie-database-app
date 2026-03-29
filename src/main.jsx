36c6246185e167bcc9d408fe68df821430e6d505
Jeffer512 <140467995+Jeffer512@users.noreply.github.com>
2026-03-29 12:47:16 -0500

    update main.jsx basename parameter in BrowserRouter for deployment to work on gh pages

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-
+console.log(import.meta.env.BASE_URL);
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename={import.meta.env.BASE_URL} >
+  <BrowserRouter basename="/react-movie-database-app" >
     <App />
   </BrowserRouter>,
 )
